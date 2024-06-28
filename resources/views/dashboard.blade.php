e4111daffeea1508835faebfbedfd919ac980000
mdnurulmomen <mdnurulmomen.bd@gmail.com>
2024-06-29 00:24:21 +0600

    Separating admin access

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,8 +8,16 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
+                <div class="p-6 text-gray-900 flex justify-between items-center">
                     {{ __("You're logged in!") }}
+
+                    @if (auth()->user()->isAdmin())
+
+                    <x-hyperlink-blue-button href="{{ route('admin.users.index') }}">
+                        {{ __('List Users') }}
+                    </x-hyperlink-blue-button>
+
+                    @endif
                 </div>
             </div>
         </div>
