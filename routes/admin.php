6b7c16693c648dbe22b8aa7a213c85594b0f1d5b
mdnurulmomen <mdnurulmomen.bd@gmail.com>
2024-06-29 00:25:15 +0600

    Separating admin-routes from user-routes

---
+++ routes/admin.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::name('admin.')->group(function () {
+
+    Route::get('/', function () {
+        return view('welcome');
+    })->name('website');
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
+
+    Route::middleware('auth')->group(function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+        // users
+        Route::get('/users/trashed', [UserController::class, 'trashed'])->name('users.trashed');
+        Route::patch('/users/{user}/restore', [UserController::class, 'restore'])->name('users.restore')->withTrashed();
+        Route::delete('/users/{user}/delete', [UserController::class, 'delete'])->name('users.delete')->withTrashed();
+        Route::resource('users', UserController::class);
+    });
+
+    require __DIR__.'/auth.php';
+
+});
